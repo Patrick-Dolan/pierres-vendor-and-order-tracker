81188911e032ee0e9688b7ed1e381b0fc4111b3e
Patrick Dolan <dolanp1992@gmail.com>
2022-02-25 17:55:37 -0800

    add controller route for splash page

--- VendorAndOrderTracker/Controllers/HomeController.cs
+++ VendorAndOrderTracker/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ namespace VendorAndOrderTracker.Controllers
 {
   public class HomeController : Controller
   {
-
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
