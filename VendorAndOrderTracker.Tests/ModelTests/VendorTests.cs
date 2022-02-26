b2218bc36950e003bf218bf68b8d4e8fb8f1b5fb
Patrick Dolan <dolanp1992@gmail.com>
2022-02-25 17:37:54 -0800

    rename appropriate folders, files, and namespaces for new project

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,10 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName.Models;
+using VendorAndOrderTracker.Models;
 
-namespace ProjectName.Tests
+namespace VendorAndOrderTracker.Tests
 {
   [TestClass]
-  public class ClassNameTests
+  public class VendorTests
   {
     // Test methods go here
   }
