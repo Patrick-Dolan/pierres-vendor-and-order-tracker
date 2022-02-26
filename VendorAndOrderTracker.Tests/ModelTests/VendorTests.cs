15e98b1ec4f0f86e3c9605ff65f333b27ba8183e
Patrick Dolan <dolanp1992@gmail.com>
2022-02-25 18:45:15 -0800

    add GetAll method for vendors to be listed

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorAndOrderTracker.Models;
 using System;
+using System.Collections.Generic;
 
 namespace VendorAndOrderTracker.Tests
 {
@@ -35,6 +36,18 @@ namespace VendorAndOrderTracker.Tests
       Vendor newVendor = new Vendor(vendorName);
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name1 = "Tester Vendor 1";
+      string name2 = "Tester Vendor 2";
+      Vendor newVendor1 = new Vendor(name1);
+      Vendor newVendor2 = new Vendor(name2);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
