583ad1d3d409c0f2db834a6fefdc4c6f98daa81d
Patrick Dolan <dolanp1992@gmail.com>
2022-02-25 19:51:52 -0800

    add vendor controller and index view

--- VendorAndOrderTracker/Controllers/HomeController.cs
+++ VendorAndOrderTracker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
 
 namespace VendorAndOrderTracker.Controllers
 {
---
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
