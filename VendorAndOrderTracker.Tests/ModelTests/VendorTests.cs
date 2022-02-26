e55ec0dc3771f5c4d584e2a86508769de8aa7489
Patrick Dolan <dolanp1992@gmail.com>
2022-02-25 18:33:54 -0800

    add test for vendorName

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -12,5 +12,14 @@ namespace VendorAndOrderTracker.Tests
       Vendor newVendor = new Vendor("Test Vendor Name");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void ReturnName_ReturnVendorName_String()
+    {
+      string vendorName = "Test Vendor";
+      Vendor newVendor = new Vendor(vendorName);
+      string result = newVendor.Name;
+      Assert.AreEqual(vendorName, result);
+    }
   }
 }
